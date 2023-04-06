1d47969b45d3686e7ad6dd80130ae3ca9e12852b
melnichenkosa <melnichenkosa@402D-12>
2023-04-06 12:00:19 +0700

    Добавьте файлы проекта.

---
+++ Sekunden/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Sekunden
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите секунды");
+            int x = int.Parse(Console.ReadLine());
+            Zeit zeit = new Zeit(x);
+            zeit.Drucken();
+        }
+    }
+}
---
+++ Sekunden/Zeit.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sekunden
+{
+    class Zeit
+    {
+        private int sekunden;
+        private int minuten;
+        private int stunden;
+        private int tagen;
+
+        public Zeit (int sek)
+        {
+            sekunden = sek % 60;
+            int tmpminuten = (sek / 60);
+            minuten = tmpminuten % 60;
+            int tmpstunden = tmpminuten / 60;
+            stunden = tmpstunden % 24;
+            tagen = tmpstunden / 24;
+
+        }
+        public void Drucken ()
+        {
+            Console.WriteLine($"Дни: {tagen}, Часы: {stunden}, Минуты: {minuten}, Секунды: {sekunden}");
+        }
+    }
+}
