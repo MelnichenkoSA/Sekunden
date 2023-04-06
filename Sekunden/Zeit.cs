925adbda74c83d167b9bb27c820bdc9c5a48b805
MelnichenkoSA <112738977+MelnichenkoSA@users.noreply.github.com>
2023-04-06 16:11:25 +0700

    Update Zeit.cs

--- Sekunden/Zeit.cs
+++ Sekunden/Zeit.cs
@@ -6,24 +6,65 @@ namespace Sekunden
 {
     class Zeit
     {
-        private int sekunden;
-        private int minuten;
-        private int stunden;
-        private int tagen;
+         private int sek;
+        private int sekunden { get => sek % 60; }
+        private int minuten { get =>(sek / 60) % 60; }
+        private int stunden { get => ((sek / 60) / 60) % 24; }
+        private int tagen { get => ((sek / 60) / 60) / 24; }
 
         public Zeit (int sek)
         {
-            sekunden = sek % 60;
-            int tmpminuten = (sek / 60);
-            minuten = tmpminuten % 60;
-            int tmpstunden = tmpminuten / 60;
-            stunden = tmpstunden % 24;
-            tagen = tmpstunden / 24;
-
+            this.sek = sek;
         }
-        public void Drucken ()
+        public static Zeit operator + (Zeit a, Zeit b)
         {
-            Console.WriteLine($"Дни: {tagen}, Часы: {stunden}, Минуты: {minuten}, Секунды: {sekunden}");
+            int sek = a.sek + b.sek;
+            return new Zeit(sek);
+        }
+        public static Zeit operator -(Zeit a, Zeit b)
+        {
+            int sek = a.sek - b.sek;
+            return new Zeit(sek);
+        }
+        public static Zeit operator *(Zeit a, Zeit b)
+        {
+            int sek = a.sek * b.sek;
+            return new Zeit(sek);
+        }
+        public static Zeit operator /(Zeit a, Zeit b)
+        {
+            int sek = a.sek / b.sek;
+            return new Zeit(sek);
+        }
+        public static Zeit operator ++(Zeit a)
+        {
+            int sek = a.sek ++;
+            return new Zeit(sek);
+        }
+        public static Zeit operator --(Zeit a)
+        {
+            int sek = a.sek--;
+            return new Zeit(sek);
+        }
+        public static bool operator ==(Zeit a, Zeit b)
+        {
+            return a.sek == b.sek;
+        }
+        public static bool operator !=(Zeit a, Zeit b)
+        {
+            return a.sek != b.sek;
+        }
+        public static bool operator >(Zeit a, Zeit b)
+        {
+            return a.sek > b.sek;
+        }
+        public static bool operator <(Zeit a, Zeit b)
+        {
+            return a.sek < b.sek;
+        }
+        public override string ToString()
+        {
+            return $"Дни: {tagen}, Часы: {stunden:00}, Минуты: {minuten:00}, Секунды: {sekunden:00}";
         }
     }
 }
